b9146f5afc2cb8fe93664f422f8c2e12e59a7ef5
Abhishekvi09 <96611512+Abhishekvi09@users.noreply.github.com>
2022-01-21 12:22:13 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,86 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/dataDB", { useNewUrlParser: true });
+
+const con = mongoose.connection
+con.on("open",function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("connected");
+  }
+  
+});
+
+const AddSchema = {
+    Name : String,
+    Gender: String
+};
+
+const Add = mongoose.model("Add", AddSchema);
+
+
+app.route("/Add")
+
+    .get(function (req, res) {
+        Add.find(function (err, foundAdd) {
+            if (!err) {
+                res.send(foundAdd);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .post(function (req, res) {
+
+        const newAdd = new Add({
+            Name: req.body.Name,
+            Gender: req.body.Gender
+        });
+
+        newAdd.save(function(err){
+            if (!err){
+              res.send("Successfully added a new data.");
+            } else {
+              res.send(err);
+            }
+          });
+        })
+
+
+
+ app.route("/Add/:Addname")
+
+.get(function(req, res){
+
+  Add.findOne({Name: req.params.Addname}, function(err, foundAdd){
+    if (foundAdd) {
+      res.send(foundAdd);
+    } else {
+      res.send("No matching that name was found.");
+    }
+  });
+})
+
+
+
+
+
+        app.listen(3000, function () {
+            console.log("Server started on port 3000");
+        });
